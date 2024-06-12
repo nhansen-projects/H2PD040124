3d73addf9bd75b3140cc00a94a6b06c41ef646f1
ShaqTheSnack <reichsteinshaquille@gmail.com>
2024-06-12 10:36:48 +0200

    Bid History - View + ViewModel er færdige mangler bare datagrid.

---
+++ CarAuction/ViewModels/BidHistoryViewModel.cs
@@ -0,0 +1,38 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarAuction.ViewModels
+{
+    internal class BidHistoryViewModel : ViewModelBase
+    {
+        private MainWindowViewModel main;
+        public BidHistoryViewModel(MainWindowViewModel main)
+        {
+            this.main = main;
+        }
+
+        public BidHistoryViewModel()
+        {
+
+        }
+
+
+        public void HistoryList()
+        {
+
+        }
+
+        public void BackBtn()
+        {
+            main.SetViewModel(new HomePageViewModel(main));
+        }
+
+
+    }
+
+
+}
