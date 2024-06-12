5a1402d3f6932b72fdcac0cd1d92bdc200059a0e
ShaqTheSnack <reichsteinshaquille@gmail.com>
2024-06-12 10:39:00 +0200

    MyProfile - View + ViewModel er færdige.

---
+++ CarAuction/Views/MyProfileView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CarAuction.Views;
+
+public partial class MyProfileView : UserControl
+{
+    public MyProfileView()
+    {
+        InitializeComponent();
+    }
+}
