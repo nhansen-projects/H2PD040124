3f051930abe92870a6b9863ad9b3b2de18862d99
ShaqTheSnack <reichsteinshaquille@gmail.com>
2024-06-12 10:38:09 +0200

    SellerOfAuction - View + ViewModel er færdige mangler bare datagrid.

---
+++ CarAuction/ViewModels/SellerOfAuctionViewModel.cs
@@ -0,0 +1,53 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarAuction.ViewModels
+{
+    internal class SellerOfAuctionViewModel : ViewModelBase
+    {
+        private MainWindowViewModel main;
+        public SellerOfAuctionViewModel(MainWindowViewModel main)
+        {
+            this.main = main;
+        }
+
+        public SellerOfAuctionViewModel()
+        {
+
+        }
+
+
+        private string _closingTime;
+        public string ClosingTime
+        {
+            get => _closingTime;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _closingTime, value, nameof(ClosingTime));
+            }
+        }
+
+
+        private string _currentBid;
+        public string CurrentBid
+        {
+            get => _currentBid;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _currentBid, value, nameof(CurrentBid));
+            }
+        }
+
+
+        public void BackBtn()
+        {
+            //main.SetViewModel(new HomePageViewModel(main)); VED IKKE OM DET ER TIL HomePage eller hvor Seller of auction skal være?
+        }
+
+
+    }
+}
---
+++ CarAuction/Views/SellerOfAuctionView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CarAuction.Views;
+
+public partial class SellerOfAuctionView : UserControl
+{
+    public SellerOfAuctionView()
+    {
+        InitializeComponent();
+    }
+}
